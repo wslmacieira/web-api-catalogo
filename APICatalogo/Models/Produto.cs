6a52b5d92b6f23e0cdfba10aaf3cd4c8147898bc
wslmacieiradev <wslmacieira@outlook.com>
2020-11-29 15:46:58 -0300

    criado Models de Categoria e Produto

---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
