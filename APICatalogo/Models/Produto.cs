83ca14d53a7859fac2cb7959ece37b16ce338db0
wslmacieiradev <wslmacieira@outlook.com>
2020-11-29 17:32:05 -0300

    cria migrations; adiciona dataAnottations

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string Descricao { get; set; }
+        [Required]
         public decimal Preco { get; set; }
+        [Required]
+        [MaxLength(500)]
         public string ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
