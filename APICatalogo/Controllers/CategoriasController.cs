5369f257a44d4bbd9100fca4e0d934f04c8a44f2
wslmacieiradev <wslmacieira@outlook.com>
2020-12-05 00:48:39 -0300

    cria CategoriasController

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,76 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+    private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+            return categoria;
+
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria",
+                new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+
+            if( categoria == null )
+            {
+                return NotFound();
+            }
+
+            _context.Remove(categoria);
+            _context.SaveChanges();
+            return categoria;
+        }
+    }
+}
