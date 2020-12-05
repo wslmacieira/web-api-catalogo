4a7353f1b8ce1ea50bea0f88b03856f39bcb3b57
wslmacieiradev <wslmacieira@outlook.com>
2020-12-04 22:34:41 -0300

    cria ProdutosController e lista produtos

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.ToList();
+        }
+    }
+}
