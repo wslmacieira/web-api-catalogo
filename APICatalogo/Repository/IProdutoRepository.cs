8e4b389ca00e7e1c5a0e28dae160fdec5725e367
wslmacieiradev <wslmacieira@outlook.com>
2020-12-08 00:24:46 -0300

    repositório implementação

---
+++ APICatalogo/Repository/IProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Repository
+{
+    interface IProdutoRepository : IRepository<Produto>
+    {
+        IEnumerable<Produto> GetProdutoPorPreco();
+    }
+}
