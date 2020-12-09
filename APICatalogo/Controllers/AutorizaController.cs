63be448ce46510a7b31b2514e8f1318d26304ae4
wslmacieiradev <wslmacieira@outlook.com>
2020-12-09 15:36:01 -0300

    adiciona AutorizaController e  UsuarioDTO

---
+++ APICatalogo/Controllers/AutorizaController.cs
@@ -0,0 +1,73 @@
+﻿using APICatalogo.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AutorizaController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizaController :: Acessado em : "
+                + DateTime.Now.ToLongDateString();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO usuario)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+            //}
+
+            var user = new IdentityUser
+            {
+                UserName = usuario.Email,
+                Email = usuario.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuario.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO usuario)
+        {
+            var result = await _signInManager.PasswordSignInAsync(usuario.Email,
+                usuario.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login Inválido...");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
